a111a64b5943d8ccae4b39a84a54d7b4da3e70d1
lionheart21st <237725015@qq.com>
2021-03-09 23:38:12 +0800

    代码优化

--- 业务文件的批量归并/业务文件批量归并_改进版.py
+++ 业务文件的批量归并/业务文件批量归并_改进版.py
@@ -2,8 +2,8 @@ import os
 import re
 import pandas as pd
 
-# path = "./source_data"    #为了方便调试，可以将path赋值为当前路径，把该代码模块 放到图片的相同目录中 运行即可
-path=input("请输入路径：")
+path = "./source_data"    #为了方便调试，可以将path赋值为当前路径，把该代码模块 放到图片的相同目录中 运行即可
+# path=input("请输入路径：")
 
 # print(os.listdir(path))   #查看目标目录下的文件
 
@@ -31,15 +31,18 @@ def files_merge(shop_name_set):
         print(name)
         print(path+"/"+name+'广告销售.xlsx')  #拼接文件路径
         # print(os.path.exists(path+"/"+name+'广告销售.xlsx'))
-        if os.path.exists(path+"/"+name+'广告销售.xlsx') and os.path.exists(path+"/"+name+'销售总额.xlsx'):  #判断相应店名的广告销售、销售总额Excel表格文件是否存在
+        if os.path.exists(path+"/"+name+'广告销售.xlsx') :  #判断相应店名的广告销售Excel表格文件是否存在
             # print("ok")
             os.mkdir(path+"/"+name)    #以店名 新建子目录
             os.rename(path+"/"+name+'广告销售.xlsx',path+"/"+name+"/"+name+'广告销售.xlsx') #改变目标文件目录 即将目标文件放入相应的子目录中（其实就是在路径上加一层目录）
-            os.rename(path + "/" + name + '销售总额.xlsx', path + "/" + name + "/" + name + '销售总额.xlsx') #改变目标文件目录 即将目标文件放入相应的子目录中（其实就是在路径上加一层目录）
             df_log.loc[i]=[path+"/"+name,path+"/"+name+"/"+name+'广告销售.xlsx']  #添加记录到《归并信息汇总.xlsx》
-            df_log.loc[i+1] = [path + "/" + name, path + "/" + name + "/" + name + '销售总额.xlsx']#添加记录到《归并信息汇总.xlsx》
-            i+=2
-
+            i+=1
+        if os.path.exists(path+"/"+name+'销售总额.xlsx'):    #判断相应店名的销售总额Excel表格文件是否存在
+            if os.path.exists(path+"/"+name)== False :    #如果店名目录不存在，则 以店名新建子目录
+                os.mkdir(path + "/" + name)
+            os.rename(path + "/" + name + '销售总额.xlsx',path + "/" + name + "/" + name + '销售总额.xlsx')  # 改变目标文件目录 即将目标文件放入相应的子目录中（其实就是在路径上加一层目录）
+            df_log.loc[i] = [path + "/" + name, path + "/" + name + "/" + name + '销售总额.xlsx']  # 添加记录到《归并信息汇总.xlsx》
+            i+=1
     excel_name= path+'/'+'归并信息汇总.xlsx'   #指定《归并信息汇总.xlsx》的路径
     df_log.to_excel(excel_name) #生成《归并信息汇总.xlsx》
 
